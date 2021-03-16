ffcdfeb01ad027e12fc166ae434f37fd1c4746d2
mu <59917266+4eUeP@users.noreply.github.com>
2021-03-16 17:19:13 +0800

    add tests & ci

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,1 +1,18 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+import           Control.Monad   (void)
+import           Test.Hspec
+import           ZooKeeper
+import           ZooKeeper.Types
+
+main :: IO ()
+main = hspec $ do
+  describe "zookeeper get set" $ do
+    it "set some value to a test node and get it" $ do
+      let res = zookeeperResInit "127.0.0.1:2182" 5000 Nothing 0
+      withResource res $ \zh -> do
+        let nodeName = "/test-node"
+        void $ zooCreate zh nodeName "" zooOpenAclUnsafe ZooPersistent
+        void $ zooSet zh nodeName "hello" Nothing
+        (dataCompletionValue <$> zooGet zh nodeName) `shouldReturn` "hello"
+        zooDelete zh nodeName Nothing `shouldReturn` ()
